a7a168a937352398190c58d9b84a24200af9643d
itkq <kuogsi@gmail.com>
2017-03-26 15:45:49 +0900

    Fix rails recipe

--- cookbooks/rails/default.rb
+++ cookbooks/rails/default.rb
@@ -9,8 +9,10 @@ include_cookbook 'libxml2::dev'
 
 execute "Install nokogiri" do
   command "gem install nokogiri -- use-sytem-libraries"
+  not_if 'gem list | grep -q nokogiri'
 end
 
 execute 'Install rails' do
   command "gem install rails -v #{node[:rails][:version]}"
+  not_if 'gem list | grep -q rails'
 end
