b2efc7e1aab087b374dddb820745243af1fa296b
itkq <kuogsi@gmail.com>
2017-04-01 20:28:29 +0900

    Update recipe: timezone
    Fix bug

--- cookbooks/timezone/default.rb
+++ cookbooks/timezone/default.rb
@@ -1,5 +1,9 @@
+node.reverse_merge!(
+  timezone: 'Asia/Tokyo'
+)
+
 link '/etc/localtime' do
-  to "/usr/share/zoneinfo/Azia/Tokyo"
+  to "/usr/share/zoneinfo/#{node[:ubuntu_timezone]}"
   force true
   notifies :run, 'execute[dpkg-reconfigure tzdata]'
 end
